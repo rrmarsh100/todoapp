473b6dc339a999bdca3a5029a68a0f09668e488d
Robert Marsh <nhe6dc@gmail.com>
2017-10-04 00:35:56 -0400

    first commit in seven

---
+++ app.js
@@ -0,0 +1,36 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', (event) => {
+     event.preventDefault();
+
+     addToDoForm.addEventListener('delete', (event) => {
+        event.preventDefault();
+
+      let title = newToDoText.value;
+
+      let newLi = document.createElement('li');
+
+      let checkbox = document.createElement('input');
+
+      checkbox.type = "checkbox";
+
+      newLi.textContent = title;
+
+      newLi.appendChild(checkbox);
+
+      newLi.appendChild(checkbox);
+
+      newToDoText.value = '';
+
+  });
+
+}
+
+
+window.onload = function() {
+  alert("The window has loaded!");
+  onReady();
+};
